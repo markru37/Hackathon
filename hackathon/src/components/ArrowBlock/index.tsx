93ca68df25b45c4c546432f73bf142f21f4cec30
Mark_Rumjantsev <mrk.ru@list.ru>
2022-10-23 12:40:12 +0300

    fix code: layout

--- hackathon/src/components/ArrowBlock/index.tsx
+++ hackathon/src/components/ArrowBlock/index.tsx
@@ -13,14 +13,26 @@ const ArrowBlock: React.FC = () => {
             <div className={classes.arrow__block}>
                 <img src={arrow} alt="arrow" className={classes.arrow__block__image} />
                 <img src={testgif} alt="gif" className={classes.arrow__block__gif} />
+                <div className={classes.arrow__block__info}>Lorem ipsum dolor sit amet consectetur
+                 adipisicing elit. Culpa, pariatur repudiandae cupiditate 
+                voluptatem cum enim molestiae nam ipsum harum quos officiis
+                 ducimus, corporis eos reiciendis maiores beatae esse facilis error.</div>
             </div>
             <div className={`${classes.arrow__block} ${classes.arrow__block__rotate}`}>
                 <img src={arrow_rotate} alt="arrow" className={`${classes.arrow__block__image}`} />
                 <img src={testgif} alt="gif" className={`${classes.arrow__block__gif} ${classes.gif__rotate}`} />
+                <div className={`${classes.arrow__block__info__rotate}`}>Lorem ipsum dolor sit amet consectetur
+                 adipisicing elit. Culpa, pariatur repudiandae cupiditate 
+                voluptatem cum enim molestiae nam ipsum harum quos officiis
+                 ducimus, corporis eos reiciendis maiores beatae esse facilis error.</div>
             </div>
             <div className={classes.arrow__block}>
                 <img src={arrow} alt="arrow" className={classes.arrow__block__image} />
                 <img src={testgif} alt="gif" className={classes.arrow__block__gif} />
+                <div className={classes.arrow__block__info}>Lorem ipsum dolor sit amet consectetur
+                 adipisicing elit. Culpa, pariatur repudiandae cupiditate 
+                voluptatem cum enim molestiae nam ipsum harum quos officiis
+                 ducimus, corporis eos reiciendis maiores beatae esse facilis error.</div>
             </div>
         </div>
     )
