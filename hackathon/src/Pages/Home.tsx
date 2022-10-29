84d398ed9f0efd892d7e21bac61d8f8a45c96c0f
Mark_Rumjantsev <mrk.ru@list.ru>
2022-10-30 02:13:02 +0300

    refactor

--- hackathon/src/Pages/Home.tsx
+++ hackathon/src/Pages/Home.tsx
@@ -4,7 +4,6 @@ import classes from './Pages.module.scss'
 import Parallax from '../components/Parallax'
 import ArrowBlock from '../components/ArrowBlock'
 import Slider from '../components/Slider'
-// import Header from '../components/Header'
 import Roadmap from '../components/Roadmap'
 import FAQ from '../components/FAQ'
 import Footer from '../components/Footer'
@@ -12,17 +11,17 @@ import Footer from '../components/Footer'
 const Home: React.FC = () => {
     return (
         <>  
-            {/* <Header/> */}
             <Parallax/>
             <div className={classes.wrapper}>
                 <div className={classes._container}>
                     <ArrowBlock/>
+                    <Roadmap/>
                     <Slider/>
+                    <FAQ/>
+                    <Footer/>
                 </div>
             </div>
-            <Roadmap/>
-            <FAQ/>
-            <Footer/>
+
         </>
     )
 }
