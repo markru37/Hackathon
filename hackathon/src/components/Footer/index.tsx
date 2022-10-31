dee9dbf566ad5c5566dc15ad99a26bc554691a4c
Mark_Rumjantsev <mrk.ru@list.ru>
2022-10-31 15:42:08 +0300

    refactoring

--- hackathon/src/components/Footer/index.tsx
+++ hackathon/src/components/Footer/index.tsx
@@ -11,9 +11,9 @@ const Footer:React.FC = () => {
             <div className="footer__body wrapper">
                 <div className="footer_title">Let's stay in touch!</div>
                 <div className="footer_seo">
-                    <a href="https://vk.com/trapismyup" className="seo_butt">FOLLOW US ON VK <img className="svg_socweb-footer" src={vk} alt="vk" /></a>
-                    <a href="#" className="seo_butt" >ADD US ON TELEGRAM: @ruslanpolakov<img className="svg_socweb-footer" src={telegram} alt="telegram" /></a>
-                    <a href="https://github.com/markru37" className="seo_butt" >FOLLOW US ON GITHUB <img className="svg_socweb-footer" src={github} alt="github" /></a>
+                    <a href="https://vk.com/trapismyup" className="seo_butt">FOLLOW US ON VK <img className="svg_socweb" src={vk} alt="vk" /></a>
+                    <a href="#" className="seo_butt" >ADD US ON TG: <b className="link__tg">@ruslanpolakov</b><img className="svg_socweb" src={telegram} alt="telegram" /></a>
+                    <a href="https://github.com/markru37" className="seo_butt" >FOLLOW US ON GITHUB <img className="svg_socweb" src={github} alt="github" /></a>
                 </div>
             </div>
         </div>
