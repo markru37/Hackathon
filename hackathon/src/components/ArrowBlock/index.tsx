3d2b49b138a6d732cc6c621e10dac8d04e0e2720
SunM1sty <mis7akes@mail.ru>
2022-11-06 19:37:38 +0300

    changed the block to the video block

--- hackathon/src/components/ArrowBlock/index.tsx
+++ hackathon/src/components/ArrowBlock/index.tsx
@@ -5,37 +5,17 @@ import testgif from '../../assets/img/test1.gif'
 import arrow_rotate from '../../assets/img/arrow_rotate.png'
 
 const ArrowBlock: React.FC = () => {
-    return (
-        <div className={classes.main}>
-            <div className={classes.main__title}>
-                Lorem Ipsum
-            </div>
-            <div className={classes.arrow__block}>
-                <img src={arrow} alt="arrow" className={classes.arrow__block__image} />
-                <img src={testgif} alt="gif" className={classes.arrow__block__gif} />
-                <div className={classes.arrow__block__info}>Lorem ipsum dolor sit amet consectetur
-                 adipisicing elit. Culpa, pariatur repudiandae cupiditate 
-                voluptatem cum enim molestiae nam ipsum harum quos officiis
-                 ducimus, corporis eos reiciendis maiores beatae esse facilis error.</div>
-            </div>
-            <div className={`${classes.arrow__block} ${classes.arrow__block__rotate}`}>
-                <img src={arrow_rotate} alt="arrow" className={`${classes.arrow__block__image}`} />
-                <img src={testgif} alt="gif" className={`${classes.arrow__block__gif} ${classes.gif__rotate}`} />
-                <div className={`${classes.arrow__block__info__rotate}`}>Lorem ipsum dolor sit amet consectetur
-                 adipisicing elit. Culpa, pariatur repudiandae cupiditate 
-                voluptatem cum enim molestiae nam ipsum harum quos officiis
-                 ducimus, corporis eos reiciendis maiores beatae esse facilis error.</div>
-            </div>
-            <div className={classes.arrow__block}>
-                <img src={arrow} alt="arrow" className={classes.arrow__block__image} />
-                <img src={testgif} alt="gif" className={classes.arrow__block__gif} />
-                <div className={classes.arrow__block__info}>Lorem ipsum dolor sit amet consectetur
-                 adipisicing elit. Culpa, pariatur repudiandae cupiditate 
-                voluptatem cum enim molestiae nam ipsum harum quos officiis
-                 ducimus, corporis eos reiciendis maiores beatae esse facilis error.</div>
-            </div>
-        </div>
-    )
+	return (
+		<div className={classes.main}>
+			<div className={classes.main__title}>
+				Видео-презентация
+			</div>
+			<div className={classes.arrow__block}>
+				<span className="span__animate"></span>
+				<iframe className='box__animate' width="1280" height="720" src="https://www.youtube.com/embed/6TYkDy54q4E" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"></iframe>
+			</div>
+		</div>
+	)
 }
 
 export default ArrowBlock
